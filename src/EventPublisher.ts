69b0d00eccdb661f5c6e9f11f52906b9d70a20e7
Oren (electricessence) <oren@electrified.net>
2020-04-27 00:42:13 -0700

    Improved flexibility.

--- src/EventPublisher.ts
+++ src/EventPublisher.ts
@@ -19,7 +19,10 @@ export interface Event<T> extends Readonly<IOrderedRegistry<Listener<T>>>
 
 export interface IEventPublisher<T>
 {
-	publish(payload: T): void;
+	publish(payload: T, reverse?: boolean): void;
+
+	publishForward(payload: T): void;
+
 	publishReverse(payload: T): void;
 
 	readonly event: Readonly<Event<T>>;
@@ -27,8 +30,37 @@ export interface IEventPublisher<T>
 
 export class EventPublisher<T> implements IEventPublisher<T>
 {
-	private readonly _registry: OrderedRegistry<Listener<T>>;
+	protected readonly _registry: OrderedRegistry<Listener<T>>;
 	private readonly _event: Event<T>;
+
+	constructor(
+		public clearListenersAfterPublish: boolean = false,
+		public remaining: number = Number.POSITIVE_INFINITY)
+	{
+		const r = new OrderedRegistry<Listener<T>>();
+		this._registry = r;
+		const add = (listener: Listener<T>) =>
+		{
+			return this.remaining > 0 ? r.add(listener) : NaN;
+		};
+		const remove = (id: number) => r.remove(id);
+		const event = (listener: Listener<T>) =>
+		{
+			const id = add(listener);
+			return isNaN(id) ? dummy : () =>
+			{
+				remove(id)
+			};
+		};
+		event.add = add;
+		event.remove = remove;
+		event.register = (listener: Listener<T>) =>
+		{
+			return this.remaining > 0 ? r.register(listener) : NaN;
+		};
+		event.clear = () => r.clear();
+		this._event = Object.freeze(event);
+	}
 
 	/**
 	 * The event object to subscribe to.
@@ -41,13 +73,30 @@ export class EventPublisher<T> implements IEventPublisher<T>
 	/**
 	 * Dispatches payload to listeners.
 	 * @param payload
+	 * @param reverse
 	 */
-	publish(payload: T): void
+	publish(payload: T, reverse: boolean = false): void
 	{
-		this._registry.forEach((listener:Listener<T>) =>
+		let r = this.remaining;
+		if (isNaN(r) || r <= 0) return;
+		if (isFinite(r)) r = --this.remaining;
+		if (reverse) this._registry.forEachReverse(f);
+		else this._registry.forEach(f);
+		if (r == 0 || this.clearListenersAfterPublish) this._registry.clear();
+
+		function f(listener: Listener<T>)
 		{
 			listener(payload);
-		});
+		}
+	}
+
+	/**
+	 * Dispatches payload to listeners.
+	 * @param payload
+	 */
+	publishForward(payload: T): void
+	{
+		this.publish(payload);
 	}
 
 	/**
@@ -56,28 +105,10 @@ export class EventPublisher<T> implements IEventPublisher<T>
 	 */
 	publishReverse(payload: T): void
 	{
-		this._registry.forEachReverse((listener:Listener<T>) =>
-		{
-			listener(payload);
-		});
-	}
-
-	constructor()
-	{
-		const r = new OrderedRegistry<Listener<T>>();
-		this._registry = r;
-		const add = (listener: Listener<T>) => r.add(listener);
-		const remove = (id: number) => r.remove(id);
-		const event = (listener: Listener<T>) => {
-			const id = add(listener);
-			return ()=>{remove(id)};
-		};
-		event.add = add;
-		event.remove = remove;
-		event.register = (listener: Listener<T>) => r.register(listener);
-		event.clear = () => r.clear();
-		this._event = Object.freeze(event);
+		this.publish(payload, true);
 	}
 }
 
-export default EventPublisher;
+function dummy()
+{
+}
