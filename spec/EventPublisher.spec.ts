9b1df653fb8fd07e21b1c91fe4e746901cb3002d
Oren (electricessence) <oren@electrified.net>
2020-04-27 10:00:30 -0700

    Added specs.

--- spec/EventPublisher.spec.ts
+++ spec/EventPublisher.spec.ts
@@ -7,5 +7,37 @@ import EventPublisher from '../src/EventPublisher';
 
 describe('EventPublisher', () =>
 {
+	it('should publish events while remaining is positive', () =>
+	{
+		let count = 0;
+		const max = 3;
+		const pub = new EventPublisher<void>(max);
+		pub.event.add(() => ++count);
+		for (let i = 1; i <= max; i++)
+		{
+			pub.publish();
+			expect(count).toBe(i);
+		}
+		pub.publish();
+		expect(count).toBe(3);
+	});
+
+	it('when configured, should clear event registration for every publish', () =>
+	{
+		let count = 0;
+		const max = 3;
+		const pub = new EventPublisher<void>(max);
+		pub.clearListenersAfterPublish = true;
+		for (let i = 1; i <= max; i++)
+		{
+			pub.event.add(() => ++count);
+			pub.publish();
+			expect(count).toBe(i);
+		}
+		pub.publish();
+		expect(count).toBe(3);
+	});
 
 });
+
+
